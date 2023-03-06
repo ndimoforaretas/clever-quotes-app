426579338e9774990322f66cf19387f8662098a0
Ndimofor <ndimaret@gmail.com>
2023-03-06 11:17:45 +0100

    add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Button, Card } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>Hellooooooooooo</h1>
-      <p>Netlify!</p>
-    </div>
+    <Card>
+      <Card.Header>Clever Quote</Card.Header>
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex,
+            dignissimos placeat dolorem esse et error reiciendis nulla ratione.
+            Quas ad totam aspernatur, saepe cumque deserunt impedit autem quo
+            blanditiis reiciendis.
+          </p>
+          <footer className="blockquote-footer">
+            Someone famous <cite title="Source Title">Source Title</cite>
+          </footer>
+        </blockquote>
+        <Button className="primary">Nue Quote</Button>
+      </Card.Body>
+    </Card>
   );
 }
 
